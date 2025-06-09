90a27145e3898f7ce727b8d4b5922f848d4f517b
Italo Espinoza <Italoespinoza1529@gmail.com>
2025-06-08 21:58:52 -0500

    Italo Espinoza - Api gateway

---
+++ src/main/java/com/espinoza/EspinozaT1ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.espinoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspinozaT1ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspinozaT1ApiGatewayApplication.class, args);
+	}
+
+}
